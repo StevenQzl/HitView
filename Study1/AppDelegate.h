288195805aeadc54b85876f97cec53d0578e51ef
StevenQzl <m18103842369@163.com>
2020-03-17 14:18:23 +0800

    Initial Commit

---
+++ Study1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Study1
+//
+//  Created by 屈亮 on 2020/3/17.
+//  Copyright © 2020 屈亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
